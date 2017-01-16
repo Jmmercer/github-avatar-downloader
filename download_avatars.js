baf0a1dfa8571b0b45e8bcbea6099d05dc732b5c
Jeffrey <Jmmercer@ucalgary.ca>
2017-01-16 22:06:53 +0000

    added dlfunction

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,13 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = 'Jmmercer';
 var UA = 'Jmmercer'
 var GITHUB_TOKEN = '14eabf81f8ba895f6abf4d7ab0120553c442811f';
 
 
 console.log('Welcome to the GitHub Avatar Downloader');
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log('Requested URL: ' + requestURL)
@@ -20,8 +23,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, GITHUB_USER, function(err, response, body){
     if (err) throw err;
     console.log('Response Status Code: ', response.statusCode);
+    //console.log(body);
+    JSON.parse(body, {}).forEach(function(contributor){
+      console.log(contributor.avatar_url)
+    });
+    //console.log(JSON.parse(body, {}))
   });
+}
 
+function downloadImageByURL(url, filePath) {
+  request.get(avatar_url)
+    .on('error', function (err){
+      throw err;
+    })
+    .on('response', function (response){
+      console.log('Downloaded')
+    })
+    .pipe(fs.createWriteStream('filePath'));
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
